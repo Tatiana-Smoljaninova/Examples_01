04312315de27b9dcb79fe891003f0fc1adaf8526
Tatiana-Smoljaninova <119117256+Tatiana-Smoljaninova@users.noreply.github.com>
2022-12-04 11:39:59 +0600

    Save changes in GitHub

--- Examples_01/Examples_Hello_console/Program.cs
+++ Examples_01/Examples_Hello_console/Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine("2 Hello, World!" Программа консоль - это мой мир и я его люблю);
+﻿Console.WriteLine("2 Hello, World!" Программа консоль - это мой мир и я его люблю - это я добавила в Git_Hub);
